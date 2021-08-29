75af9ec5f413527cdb76cb23e9dbefeff1fb2200
Ferrazza <jose.ferrazza@dxc.com>
2021-08-29 11:39:42 -0700

    aula de comparações, template e string e ternario

---
+++ comparacoes.js
@@ -0,0 +1,19 @@
+const n =5;
+const t = "5";
+
+console.log(n==t);
+console.log(n===t);
+
+//tipo da variavel
+console.log(typeof n);
+console.log(typeof t);
+
+//conversoes
+console.log(n===Number(t));
+console.log(String(n)===t);
+
+//Operadores
+// ||  - or
+// &&  - and
+// !=  - diferente (mesmo efeito do ==)
+// 1== - estritamente diferente (mesmo efeito do ===)
---
+++ template-string.js
@@ -0,0 +1,10 @@
+const nome = 'ju';
+const idade = 15;
+const cidade = "São Carlos"
+const apresentacao = `meu nome e ${nome}, minha idade é ${idade} e nasci na cidade ${cidade}`
+
+console.log(apresentacao);
+
+const apresentacao2 = idade >= 18 ?`meu nome é ${nome} e eu gostaria de beber cerveja`: `meu nome é ${nome} e eu gostaria de beber suco` 
+
+console.log(apresentacao2);
