b46c753e83a2b262cd546424db2aecb67ac4e326
Ferrazza <jose.ferrazza@dxc.com>
2021-09-01 16:45:40 -0700

    novo treino com .map

--- forEach.js
+++ forEach.js
@@ -9,3 +9,48 @@ notas.forEach(notas => {
 let media = soma / notas.length
 
 console.log(`A média é ${media}`)
+
+// dado um array, retornar outro array que adicione um ponto às notas
+
+
+//usando function
+function pontoAdicional (nota) {
+    if (nota != 10) {
+        nota += 1
+    }
+    return nota
+}
+
+const novoArrayNotas = notas.map(pontoAdicional)
+console.log('Usando function')
+console.log(notas)
+console.log(novoArrayNotas)
+
+//usando arrow function
+const novoArrayNotasAF = notas.map(nota => {
+    if (nota != 10) {
+        nota += 1
+    }
+    return nota
+})
+console.log('Usando Arrow function')
+console.log(notas)
+console.log(novoArrayNotasAF)
+
+//usando ternarias
+const novoArrayNotasT = notas.map(nota => nota!=10 ? nota += 1: nota )
+console.log('Usando ternarias')
+console.log(notas)
+console.log(novoArrayNotasAF)
+
+//multiplicando por 10
+
+const arrayNums = [1, 2, 3, 4]
+
+const novoArrayNums = arrayNums.map(notas => {
+    notas *= 10
+    return notas
+})
+
+console.log(arrayNums)
+console.log(novoArrayNums)
