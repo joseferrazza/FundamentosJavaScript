94a9f1f565ba5d80d1ccc3b7862ef92f0e1fc9e1
Ferrazza <jose.ferrazza@dxc.com>
2021-09-10 18:36:39 -0700

    funcoes em objetos

--- objetos.js
+++ objetos.js
@@ -30,4 +30,59 @@ delete cliente.idade
 
 console.log(cliente)
 
+//chaves que contem arrays
+const pessoa = {
+    nome: "zé",
+    idade: 45,
+    cpf: "27323133866",
+    email: "jose.ferrazza@gmail.com",
+    fone: ["16 99601-2935","16 3307-4204"]
+}
 
+console.log(pessoa)
+
+pessoa.fone.forEach(chave => console.log(chave))
+
+//chaves que sao objetos
+pessoa.dependente = {
+    nome: "Pedro",
+    parentesco: "filho"
+}
+
+console.log(pessoa)
+console.log(pessoa.dependente.nome)
+
+//criando arrays de objetos
+delete pessoa.dependente
+
+pessoa.dependente = [{
+    nome: "Pedro",
+    parentesco: "filho"
+}]
+pessoa.dependente.push({
+    nome: "Sofia",
+    parentesco: "filha"
+})
+
+console.log(pessoa)
+
+const mulher = pessoa.dependente.filter(dependente => dependente.nome === "Sofia")
+
+console.log(mulher)
+
+//funcoes
+const contabancaria = {
+    nome: "zé",
+    idade: 45,
+    cpf: "27323133866",
+    email: "jose.ferrazza@gmail.com",
+    fone: ["16 99601-2935","16 3307-4204"],
+    saldo: 100,
+    depositar: function (valor){
+        this.saldo += valor
+    }
+}
+
+console.log(contabancaria.saldo)
+contabancaria.depositar(30)
+console.log(contabancaria.saldo)
